2ec8ea7c2c60a7a30679dffdae221b56426b6d13
sora100fuegos <cienfuegossilvacarlos@gmail.com>
2017-10-16 15:28:58 -0500

    chcicharronera agregada

--- semana4/src/semana4/funcion.java
+++
@@ -1,5 +0,0 @@
-package semana4;
-
-public class funcion {
-
-}
---
+++ semana4/src/semana4/funcionp.java
@@ -0,0 +1,30 @@
+package semana4;
+
+public class funcionp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int x=10;
+		int y=9;
+		
+		
+		float  num1=  10f;
+		float  num2= 9.4f;
+		
+		float res= num1+x;
+		double  a=34;
+		double b=23.4;
+     double c =20;
+				
+		System.out.println("el resultado de  la operacion  de la suma de   " +x +"+"+  y  + "es "+x+y);
+		System.out.println("el resultado de  la operacion  de la suma de   " +num2+x +" el segundo resultado e"+  res);
+		double x2= (  (-b)-  Math.sqrt(Math.pow(b, 2)  -4*(c*a)))/(2*a) ;
+		
+		System.out.println("chicharronera"+x2);
+		
+		
+	}
+
+}
